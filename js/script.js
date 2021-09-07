d962ae0c305cc656245542081d7448de05f3b628
Inna <innlinag@gmail.com>
2021-09-07 22:51:10 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+const answers = [];
+
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('How old are you?', '');
+answers[2] = prompt('Where are you from?', '');
+
+document.write(answers);
