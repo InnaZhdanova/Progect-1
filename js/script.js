471d6c39f3c3bcaf053043d53d2f85e46096858c
Inna <innlinag@gmail.com>
2021-09-25 14:51:12 +0300

    Lesson 11. Homework

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 
 const numberOfFilms = +prompt('How many films have you alredy wached?', '');
 
-eval.document.write('You have already wathed ' + numberOfFilms + ' films');
-
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -11,3 +9,13 @@ const personalMovieDB = {
   genres: [],
   privat: false
 };
+
+const movie1 = prompt('One of the last wathed films:', ''),
+      rate1 = prompt('How much do you rate it?', ''),
+      movie2 = prompt('One more of the last wathed films:', ''),
+      rate2 = prompt('How much do you rate it?', '');
+
+personalMovieDB.movies[movie1] = rate1;
+personalMovieDB.movies[movie2] = rate2;
+
+console.log(personalMovieDB);
